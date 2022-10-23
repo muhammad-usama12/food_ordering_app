b749f2f912c8fd05a766c4ad939d62e1dc460989
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-23 22:24:01 +0000

    add some data to tables and change seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds
-INSERT INTO users (name, phone) VALUES ('Alice', 4163456789);
-INSERT INTO users (name, phone) VALUES ('Kira', 9058649254);
-INSERT INTO users (name, phone) VALUES ('Andy', 6478940394);
+INSERT INTO users (name, phone, email, password) VALUES ('Alice', 4163456789, 'alice@gmail.com', 'password');
+INSERT INTO users (name, phone, email, password) VALUES ('Kira', 9058649254, 'kira@gmail.com', 'password');
+INSERT INTO users (name, phone, email, password) VALUES ('Andy', 6478940394, 'andy@gmail.com', 'password');
--- db/seeds/02_menu_items.sql
+++ db/seeds/02_menu_items.sql
@@ -1,10 +1,10 @@
 -- Menu table seeds
-INSERT INTO menu_items (name) VALUES ('burger');
-INSERT INTO menu_items (name) VALUES ('taco');
-INSERT INTO menu_items (name) VALUES ('pasta');
-INSERT INTO menu_items (name) VALUES ('fries');
-INSERT INTO menu_items (name) VALUES ('chips');
-INSERT INTO menu_items (name) VALUES ('side salad');
-INSERT INTO menu_items (name) VALUES ('soda');
-INSERT INTO menu_items (name) VALUES ('milk');
-INSERT INTO menu_items (name) VALUES ('water');
+INSERT INTO menu_items (name, type) VALUES ('burger', 'main');
+INSERT INTO menu_items (name, type) VALUES ('taco', 'main');
+INSERT INTO menu_items (name, type) VALUES ('pasta', 'main');
+INSERT INTO menu_items (name, type) VALUES ('fries', 'side');
+INSERT INTO menu_items (name, type) VALUES ('chips and guac', 'side');
+INSERT INTO menu_items (name, type) VALUES ('side salad', 'side');
+INSERT INTO menu_items (name, type) VALUES ('soda', 'drink');
+INSERT INTO menu_items (name, type) VALUES ('milk', 'drink');
+INSERT INTO menu_items (name, type) VALUES ('water', 'drink');
