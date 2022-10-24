c3499ef64001452879c20faaf704053c414ddfd6
your_github_username <muhammad.usama12@hotmail.com>
2022-10-24 01:09:25 +0000

    install cookie-session package

--- server.js
+++ server.js
@@ -1,54 +1,68 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
-const morgan = require('morgan');
+const sassMiddleware = require("./lib/sass-middleware");
+const express = require("express");
+const morgan = require("morgan");
+const cookieSession = require("cookie-session");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  '/styles',
+  "/styles",
   sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
+    source: __dirname + "/styles",
+    destination: __dirname + "/public/styles",
     isSass: false, // false => scss, true => sass
   })
 );
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["session"],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
-const menuRoutes = require('./routes/menu');
-const cartRoutes = require('./routes/cart');
-
+const userApiRoutes = require("./routes/users-api");
+const widgetApiRoutes = require("./routes/widgets-api");
+const menuRoutes = require("./routes/menu");
+const cartRoutes = require("./routes/cart");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/menu', menuRoutes);
-app.use('/cart', cartRoutes);
+app.use("/api/users", userApiRoutes);
+app.use("/api/widgets", widgetApiRoutes);
+app.use("/menu", menuRoutes);
+app.use("/cart", cartRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/");
 });
 
 app.listen(PORT, () => {
