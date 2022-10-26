6ace29d8dad9be592aa917b67f443237f595a150
your_github_username <muhammad.usama12@hotmail.com>
2022-10-26 05:04:48 +0000

    add sendTextMessage function and export it to server.js

--- twilio.js
+++ twilio.js
@@ -1,8 +1,19 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-client.messages
-.create({body: 'Hi there', from: '+13238949640', to: '+16475407230'})
-.then(message => console.log(message.sid));
+// client.messages
+// .create({body: 'Hi there', from: '+13238949640', to: '+16475407230'})
+// .then(message => console.log(message.sid));
 
+const sendTextMessage = function (name, phone) {
+  client.messages.create({
+    body: `Hi ${name}, we have received your order!`,
+    to: phone,
+    from: +13238949640
+  })
+  .then(message => console.log('confirmation message id:', message.sid))
+    .catch(err => {
+      console.log('message not sent:', err)
+    });
+  }
+
+module.exports = { sendTextMessage };
