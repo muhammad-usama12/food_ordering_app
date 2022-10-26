d258fafd6ba6f593f7734cfef84712b6ab480451
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-26 16:21:17 +0000

    add post route to access createOrder db query

--- routes/cart.js
+++ routes/cart.js
@@ -1,15 +1,17 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
+const cartQueries = require('../db/queries/cart');
 
 const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render('cart');
+});
+
+router.post("/", (req, res) => {
+  const userId = req.session.user_id;
+  cartQueries.createOrder(userId, null).then((results) => {
+    res.render('cart');
+  })
 });
 
 
