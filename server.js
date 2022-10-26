961fed7c1e6bc8a937ff2319bbf42dcf6be7840c
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-26 15:25:23 +0000

    require and use body-parser

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const morgan = require("morgan");
 const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser")
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -16,7 +18,8 @@ app.set("view engine", "ejs");
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
+app.use(urlencodedParser);
 app.use(
   "/styles",
   sassMiddleware({
