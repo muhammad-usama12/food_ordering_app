2da87e5d0de353cd0a930756892f5e40f64bb688
Michaeltcdavis <michaeltcdavis@gmail.com>
2022-10-26 13:56:54 +0000

    refactor menu script

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,68 +1,68 @@
 $(document).ready(function() {
 
+  loadMenu();
 
-  $('article.menu-item').on('click', '.add-to-cart', () => {
-    const menuItemData = $('article.menu-item') //NEED TO ACCESS MENU ITEM DATA AND COOKIE HERE
+  $('article.menu-item').on('click', '.add-to-cart', function() {
+    const menuItemData = $(this).find('p.menu_id').text()
+    console.log('this: ', this);
+    console.log('menuItemData: ', menuItemData);
     $.ajax({
       url: `/cart`,
       method: "POST",
       data: menuItemData
     })
     .catch((e) => console.log('/cart post err: ', e.message))
-  })
-
-  // Create's menu objects that matches key.value pairs from the database
-  createMenuElement = function(menuObject) {
-    console.log('test');
-    const $menu = $('<article class="menu-item">');
-    const $menuId = $('<p></p>');
-    const $menuName = $('<h2></h2>');
-    const $menuPhoto = $('<p class="photo_url"></p>');
-    const $menuAddToCart = $('<i class="add-to-cart fa-sharp fa-solid fa-plus"></i>');
-
-    $menuName.text(menuObject.name);
-    console.log("menu object name:", menuObject.name);
-    $menuPhoto.text(menuObject.photo_url);
-    $menuId.text(menuObject.id);
-
-    $menu.append($menuId);
-    $menu.append($menuName);
-    $menu.append($menuPhoto);
-    $menu.append($menuAddToCart);
-    return $menu;
-  };
-
-
-
-  //
-  renderMenu = function (menus) {
-    console.log('render menu test')
-    $("#menu-container").html("");
-    for (let menu of menus) {
-      const $menu = createMenuElement(menu);
-      $("#menu-container").append($menu);
-    }
-  };
-
-  //
-  const loadMenu = function () {
-    console.log('load menu test')
-    $.ajax({
-      url: `/api/menu`,
-      method: "GET",
-    })
-    .then(function (result) {
-      console.log("load menu result", result);
-      renderMenu(result);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-  };
-
-  loadMenu();
+  });
 
 });
 
+// Create's menu objects that matches key.value pairs from the database
+createMenuElement = function(menuObject) {
+  console.log('test');
+  const $menu = $('<article class="menu-item">');
+  const $menuId = $('<p class="menu_id"></p>');
+  const $menuName = $('<h2></h2>');
+  const $menuPhoto = $('<p class="photo_url"></p>');
+  const $menuAddToCart = $('<i class="add-to-cart fa-sharp fa-solid fa-plus"></i>');
+
+  $menuName.text(menuObject.name);
+  console.log("menu object name:", menuObject.name);
+  $menuPhoto.text(menuObject.photo_url);
+  $menuId.text(menuObject.id);
+
+  $menu.append($menuId);
+  $menu.append($menuName);
+  $menu.append($menuPhoto);
+  $menu.append($menuAddToCart);
+  return $menu;
+};
+
+
+
+//
+renderMenu = function (menus) {
+  console.log('render menu test')
+  $("#menu-container").html("");
+  for (let menu of menus) {
+    const $menu = createMenuElement(menu);
+    $("#menu-container").append($menu);
+  }
+};
+
+//
+const loadMenu = function () {
+  console.log('load menu test')
+  $.ajax({
+    url: `/api/menu`,
+    method: "GET",
+  })
+  .then(function (result) {
+    console.log("load menu result", result);
+    renderMenu(result);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+};
 
 
