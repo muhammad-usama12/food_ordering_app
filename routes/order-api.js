c334310b4c591ed90d5f60aedc17ebcb2d0ed8f8
your_github_username <muhammad.usama12@hotmail.com>
2022-10-27 17:38:10 +0000

    add sendTextMessage function for twilio notification

--- routes/order-api.js
+++ routes/order-api.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { checkAdmin } = require("../db/queries/menu");
 const router = express.Router();
 const orderQueries = require('../db/queries/order');
+const { sendTextMessageCustomer } = require("../twilio");
 
 router.get("/", (req, res) => {
   const userId = req.session.user_id;
+  const fill_time = req.body.fill_time;
   console.log('userId', userId);
   checkAdmin(userId)
     .then(result => {
@@ -17,8 +19,15 @@ router.get("/", (req, res) => {
           .catch((e) => console.log('admin get orders err: ', e.message));
       }
       return orderQueries.getOrderIdByUserId(userId)
+      .then((result) => {
+        const orderId = result.id;
+        return orderQueries.getUserByOrderId(orderId)
+      })
         .then((result) => {
-          const orderId = result.id;
+          const orderId = result.order_id;
+          const name = result.name;
+          const phone = result.phone;
+          sendTextMessageCustomer(name, phone, orderId, fill_time)
           console.log('orderId: ', orderId)
           return orderQueries.getItemsByOrderId(orderId)
         })
