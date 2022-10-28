931edacc1e63ca38ec1f583e354bc8730087de09
your_github_username <muhammad.usama12@hotmail.com>
2022-10-28 05:49:35 +0000

    change remove button for a remove icon

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,43 +1,41 @@
-$(document).ready(function() {
-
-
-  $('#cart-container').on('click', 'button.remove-item', function() {
+$(document).ready(function () {
+  $("#cart-container").on("click", "i.fa-sharp", function () {
     const $removeButton = $(event.target);
-    const $cartItem = $removeButton.closest('article.cart-item');
-    const cartId = $cartItem.find('p.cart_id').text()
+    const $cartItem = $removeButton.closest("article.cart-item");
+    const cartId = $cartItem.find("p.cart_id").text();
     $.ajax({
       url: `/api/cart/delete`,
       method: "POST",
-      data: { cartId }
-    }).then((result) => {
-      loadCart();
+      data: { cartId },
     })
-    .catch((e) => console.log('/cart/delete post err: ', e.message))
+      .then((result) => {
+        loadCart();
+      })
+      .catch((e) => console.log("/cart/delete post err: ", e.message));
   });
 
   loadCart();
-
 });
 
 //
 const loadCart = function () {
-  console.log('load cart test')
+  console.log("load cart test");
   $.ajax({
     url: `/api/cart`,
     method: "GET",
   })
-  .then(function (result) {
-    console.log("load cart result", result);
-    renderCart(result);
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+    .then(function (result) {
+      console.log("load cart result", result);
+      renderCart(result);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 };
 
 //
-renderCart = function(carts) {
-  console.log('render cart test')
+renderCart = function (carts) {
+  console.log("render cart test");
   $("#cart-container").html("");
   for (let cart of carts) {
     const $cart = createCartElement(cart);
@@ -46,16 +44,15 @@ renderCart = function(carts) {
 };
 
 // Create's cart objects that matches key.value pairs from the database
-createCartElement = function(cartObject) {
-  console.log('Cart Object:', cartObject);
+createCartElement = function (cartObject) {
+  console.log("Cart Object:", cartObject);
   const $cart = $('<article class="cart-item">');
   const $cartId = $('<p class="cart_id"></p>');
-  const $cartTimeOrdered = $('<p></p>');
-  const $cartSpecialRequest = $('<p></p>');
-  const $cartName = $('<h2></h2>');
-  const $cartPhoto = $('<p></p>');
-  const $cartDelete = $('<button type="button" class="remove-item btn">Remove</button>');
-  // const $itemDiv = $('<div class=item_container></div>')
+  const $cartTimeOrdered = $("<p></p>");
+  const $cartSpecialRequest = $("<p></p>");
+  const $cartName = $("<h2></h2>");
+  const $cartPhoto = $("<p></p>");
+  const $cartDelete = $('<a><i class="fa-sharp fa-solid fa-xmark"></i></a>');
 
   // $cart.append($itemDiv);
   $cartId.text(cartObject.id);
@@ -72,4 +69,3 @@ createCartElement = function(cartObject) {
   $cart.append($cartDelete);
   return $cart;
 };
-
